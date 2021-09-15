7b73080a9f9d80ada878c414d782710a974e81ce
marvelmikel <49599133+marvelmikel@users.noreply.github.com>
2021-09-15 03:54:57 +0100

    seconf commit

--- lib/AllScreens/mainscreen.dart
+++ lib/AllScreens/mainscreen.dart
@@ -1,9 +1,10 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_final_fields, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_final_fields, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, unnecessary_new
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:rider_app/AllWidgets/Divider.dart';
+import 'package:geolocator/geolocator.dart';
+
 
 
 class MainScreen extends StatefulWidget
@@ -22,6 +23,23 @@ class _MainScreenState extends State<MainScreen>
   Completer<GoogleMapController> _controllerGoogleMap = Completer();
   late GoogleMapController newGoogleMapController;
 
+  GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+
+   late Position currentPosition;
+  var geolocator = Geolocator();
+  double bottomPaddingOfMap = 0;
+
+  void locatePosition() async
+  {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    currentPosition = position;
+
+    LatLng latLatPosition = LatLng(position.latitude, position.longitude);
+
+    CameraPosition cameraPosition = new CameraPosition(target: latLatPosition, zoom: 14);
+    newGoogleMapController.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
@@ -30,27 +48,123 @@ class _MainScreenState extends State<MainScreen>
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
-        title: Text("Main Screen"),
+        title: Text("Home"),
+      ),
+      drawer: Container(
+        color: Colors.white,
+        width:255.0,
+        child: Drawer(
+          child: ListView(
+            children: [
+              //Drawer Header
+              Container(
+                height: 165.0,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(color:Colors.white),
+                  child: Row(
+                     children: [
+                       Image.asset("images/user_icon.png", height: 65.0, width: 65.0,),
+                       SizedBox(width: 16.0,),
+                       Column(
+                         mainAxisAlignment: MainAxisAlignment.center,
+                         children:[
+                           Text("Profile Name", style: TextStyle(fontSize: 16.0, fontFamily: "Brand Bold"),),
+                           SizedBox(height: 6.0,),
+                           Text("Visit Profile"),
+                         ],
+                       ),
+                     ],
+                  ),
+                ),
+              ),
+              DividerWidget(),
+
+              SizedBox(height:12.0,),
+
+              //Drawer Body Controllers
+              ListTile(
+                leading: Icon(Icons.history),
+                title: Text("History", style: TextStyle(fontSize: 15.0),),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Visit Profile", style: TextStyle(fontSize: 15.0),),
+              ),
+              ListTile(
+                leading: Icon(Icons.info),
+                title: Text("About", style: TextStyle(fontSize: 15.0),),
+              ),
+            ],
+          ),
+        ),
       ),
       body: Stack(
         children:[
           GoogleMap(
+            padding : EdgeInsets.only(bottom: bottomPaddingOfMap),
             mapType: MapType.normal,
             myLocationButtonEnabled: true,
             initialCameraPosition: _kGooglePlex,
+            myLocationEnabled: true,
+            zoomGesturesEnabled: true,
+            zoomControlsEnabled: true,
             onMapCreated: (GoogleMapController controller)
             {
               _controllerGoogleMap.complete(controller);
               newGoogleMapController = controller;
+
+              setState((){
+                bottomPaddingOfMap = 300.0;
+              }
+              );
+
+              locatePosition();
             },
           ),
+
+          //HamburgerButton for Drawer
+          Positioned(
+            top: 45.0,
+            left: 22.0,
+            child: GestureDetector(
+              onTap:()
+              {
+               scaffoldKey.currentState!.openDrawer();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(22.0),
+                  boxShadow:[
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 6.0,
+                      spreadRadius: 0.5,
+                      offset: Offset(
+                        0.7,
+                        0.7,
+                      ),
+                    ),
+                  ],
+                ),
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child:Icon(Icons.menu, color: Colors.black,),
+                  radius: 20.0,
+                ),
+              ),
+            ),
+          ),
+
+
           Positioned(
             left: 0.0,
             right: 0.0,
             bottom: 0.0,
             child: Container(
-              height: 320.0,
+              height: 300.0,
               decoration:BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(topLeft: Radius.circular(18.0), topRight: Radius.circular(18.0)),
